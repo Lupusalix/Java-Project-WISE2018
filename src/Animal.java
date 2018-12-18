a3ed89d852592749e89c5b03abd958202f461947
Lupusalix <github@lupusalix.de>
2018-12-18 15:25:20 +0100

    Starting

--- src/Animal.java
+++ src/Animal.java
@@ -3,6 +3,27 @@ public class Animal {
     private Position pos;
     private double sight;
 
+    public Position getPos() {
+        return pos;
+    }
+
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
+
+    public double getSight() {
+        return sight;
+    }
+
+    public void setSight(double sight) {
+        this.sight = sight;
+    }
 
 
+    public Animal(Position pos, double sight) {
+        this.pos = pos;
+        this.sight = sight;
+    }
+
+    public moveTo(Position p);
 }
