3656ca531cd9164e1603be40e2e3d3f119dd2683
CreeperShift <maxhenkes@gmail.com>
2019-01-07 06:27:18 +0100

    Dummy tile comments

--- src/project/engine/tile/TilePreyDummy.java
+++ src/project/engine/tile/TilePreyDummy.java
@@ -2,6 +2,11 @@ package project.engine.tile;
 
 public class TilePreyDummy extends Tile{
 
+    /*
+    Dummy tile for multi-tile prey
+    Todo: Finish implementation
+     */
+
     TilePrey master;
 
     TilePreyDummy(TilePrey master){
