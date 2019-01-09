4db8f6e49ec45aa64ad62c708b0685aaf2238499
CreeperShift <maxhenkes@gmail.com>
2019-01-09 13:37:08 +0100

    Implemented Log levels for debug
    Signed-off-by: CreeperShift <maxhenkes@gmail.com>

---
+++ src/project/engine/misc/Misc.java
@@ -0,0 +1,20 @@
+package project.engine.misc;
+
+public class Misc {
+
+    private Misc(){}
+
+
+    public static boolean debug = true;
+
+
+    /*
+    0 = off
+    1 = INFO
+    2 = LOW
+    3 = MEDIUM
+    4 = HIGH
+     */
+    public static int debugLevel = 1;
+
+}
